ebd1c4242cca0ad6dd1b5fe68d4577666000323c
pedro-surf <pedroaki.55@gmail.com>
2020-07-21 18:16:18 -0300

    Add front build and public folders

---
+++ client/build/precache-manifest.480be8f02fb6eb8b734dc2d85f2e91ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94ab5b61c66d598f5067da0921a7f20d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce2be408af72e484d553",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fad02e11d4475f7ab945",
+    "url": "/static/css/main.a543c528.chunk.css"
+  },
+  {
+    "revision": "ce2be408af72e484d553",
+    "url": "/static/js/2.92f118d4.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.92f118d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fad02e11d4475f7ab945",
+    "url": "/static/js/main.5175e29b.chunk.js"
+  },
+  {
+    "revision": "18de4390a55bd4123f3e",
+    "url": "/static/js/runtime-main.938b69c2.js"
+  }
+]);
